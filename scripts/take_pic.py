45e6250517516c44fecb01f3b65d043aac187b7e
bendover94 <l.schmirl@pernauer.com>
2023-02-21 22:31:48 +0100

    I am NOT rebuilding my kernel, imma use windows instead

--- scripts/take_pic.py
+++ scripts/take_pic.py
@@ -8,21 +8,37 @@ import time
 # usbipd wsl list # and get BUSID from Camera
 # usbipd wsl attach --busid x-y # attaching camera to ubuntu
 # then in WSL check for video using: ls /dev/bus/usb
+# also try lsusb
 # currently not working
 
+
+
+
+## ACESSING USB-DEVICES IS NOT POSSIBLE USING WSL2 WITHOUT BUILDING YOUR OWN KERNEL
+
+# (Advice on cam&mic in WSL2) ## https://version-2.com/zh/2022/02/advice-on-camera-and-microphone-in-wsl2-ubuntu/
+
+# Video for kernel rebuild    ## https://www.youtube.com/watch?v=t_YnACEPmrM
+# commands                    ## https://agiledevart.github.io/wsl2_usb_camera.txt
+# Kernel                      ## https://github.com/microsoft/WSL2-Linux-Kernel/releases/tag/linux-msft-wsl-5.15.79.1
+
+## THIS IS TO SPICY FOR ME!!
+
 ## Camera Data
-videoDevice = 1 # webcam
+videoDevice = 0  #webcam
 camResolution_width = 640       #picture width  [px]    #800   640   320   160
 camResolution_height = 480      #picture height [px]    #600   480   240   120
 
 
 ##FUNCTIONS
 def make_and_convert_pic():     #take webcam picture and convert it to RGB
-   cam = cv2.VideoCapture(videoDevice, cv2.CAP_DSHOW)
+   cam = cv2.VideoCapture(videoDevice, cv2.CAP_V4L2)
+   #cam = cv2.VideoCapture(cv2.CAP_V4L2)
    time.sleep(0.1)
    cam.set(3, camResolution_width)                                      
    cam.set(4, camResolution_height)
    img = cam.read()[1]
+   cam.release()
    return img #cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 # save Image
